7d86265dfca9a9fd958a3746385f18759cae9b3b
Muhammed Noman <muhammedalnoman@gmail.com>
2025-09-23 06:30:10 +0600

    offer's url file doen and open notification app

--- offers/urls.py
+++ offers/urls.py
@@ -3,5 +3,7 @@ from .views import *
 
 
 urlpatterns = [
-    
+    path('subcategories/<slug:slug>/', CategoryListView.as_view()),
+    path('product/<slug:slug>/', ProductDetailView.as_view()),
+    path('coupon/<slug:slug>/', CouponDetailView.as_view()),
 ]
